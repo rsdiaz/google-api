fdf3b95652d67e228a6135342c88e7a94a7f774b
robfree <robertoserrano83@gmail.com>
2015-09-05 02:21:07 +0200

    Update files

--- bin/google-api.js
+++ bin/google-api.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
-var google = require('../lib/google-api');
+var google = require('../lib/google-api.js');
 
 if (!process.argv[2]) {
   console.error('argument is required.');
   process.exit(1);
 }
-if (process.argv[2] === '--getUrl') {
+if (process.argv[2] === '--get') {
+  var shortUrl = process.argv[3];
   var g = new google({
-    
+    apiKey: 'YOUR API KEY'
   })
+
+  g.getUrl({
+    shortUrl: shortUrl,
+    projection: '',
+    callback: function(err, data) {
+      console.log('shortUrl: ' + data.id);
+      console.log('longUrl: ' + data.longUrl);
+      process.exit(0);
+    }
+  })
+  // console.log(process.argv);
 }
